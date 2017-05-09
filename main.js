7a9e2699bc06719ccbde98264b53099044917324
Reza Ghanbari <rezapython@gmail.com>
2017-05-09 15:43:58 +0430

    nullafront slider example/css/js

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
-/**
- * Created by reza on 5/9/17.
+/*
+ Full width Slider
+ use window.loader or
+ showSlides in html page.
  */
+let slideIndex = 0;
+const showSlides = () => {
+    let timer = 2000;
+    let slides = document.getElementsByClassName("slider");
+    let dots = document.getElementsByClassName("dot");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1
+    }
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    setTimeout(showSlides, timer); // Change image every  seconds
+};
+
